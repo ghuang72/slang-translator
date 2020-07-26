f647251433146dd948846c54cb762ce4244072ca
Varun Mokashi <vmokashi2001@gmail.com>
2020-07-25 23:21:16 -0400

    test button press

--- popup.js
+++ popup.js
@@ -1,6 +1,39 @@
 let translateButton = document.getElementById("translate");
 let selectedText = document.getElementById("selectedText");
 
+function generateReplacement(originalText) {
+  // loops through dictionary
+  //run replace based on the key value map
+  var replacement;
+  var translatejson = require("./json_toronto.json");
+  for (var key in translatejson) {
+    replacement = originalText.replace(key, translatejson[key].toronto);
+  }
+
+  return replacement;
+}
+
+function translateText() {
+  var selection;
+  var replacementText;
+
+  if (window.getSelection) {
+    selection = window.getSelection();
+
+    // find the replacement text string based on the selected text
+    replacementText = generateReplacement(selection);
+
+    if (selection.rangeCount) {
+      range = selection.getRangeAt(0);
+      range.deleteContents();
+      range.insertNode(document.createTextNode(replacementText));
+    }
+  } else if (document.selection && document.selection.createRange) {
+    range = document.selection.createRange();
+    range.text = replacementText;
+  }
+}
+
 translateButton.onclick = (event) => {
   chrome.tabs.executeScript(
     {
