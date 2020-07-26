cb45ce70227adc467ee5a474c75d64402de388c7
oustan10 <oustan10@gmail.com>
2020-07-26 01:02:44 -0400

    done

--- popup.js
+++ popup.js
@@ -1,10 +1,14 @@
 let translateButton = document.getElementById('translate');
 let selectedText = document.getElementById('selectedText');
+// let messageBox = document.querySelector('notranslate_5rpu')
+const ext = chrome.extension.getBackgroundPage()
+const url = chrome.runtime.getURL('./json_toronto.json')
 
-translateButton.onclick = event => {
-	chrome.tabs.executeScript({
-		code: 'window.getSelection().toString()'
-	}, text => {
-		selectedText.innerHTML = text
+translateButton.onclick = async event => {
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, tabs => {
+		chrome.tabs.sendMessage(tabs[0].id, null)
 	})
-};
+}
